849726a4cd606bb2c37dc91732347e7047da8b51
Cedric Puchalver <cedric.puchalver@gmail.com>
2022-05-28 18:44:58 +0200

    Add missing semicolon

--- includes/class-cpuch-download-codes.php
+++ includes/class-cpuch-download-codes.php
@@ -162,7 +162,7 @@ class Cpuch_Download_Codes {
 
 		// Add ettings link to the plugins page.
 		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
-		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_plugin_action_links' )
+		$this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_plugin_action_links' );
 
 	}
 
